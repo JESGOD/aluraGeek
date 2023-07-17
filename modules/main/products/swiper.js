6b9db8d3713f23cc39e3681a2858f3380797be65
JESGOD <yonnyes@unicauca.edu.co>
2023-07-17 11:44:50 -0500

    se agrego responsividad a swiper slide en m, t, p

--- modules/main/products/swiper.js
+++ modules/main/products/swiper.js
@@ -1,33 +1,42 @@
 function detectarTamañoPantalla() {
-  
-  if (screen.width < 600) {
+  const sc = screen.width;
+
+  if (sc < 600) {
     var swiper = new Swiper(".mySwiper", {
       slidesPerView: 2,
       spaceBetween: 30,
-      speed:2500,
+      speed: 2500,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
       },
-     /*  pagination: {
+      /*  pagination: {
         el: ".swiper-pagination",
         clickable: true,
       }, */
     });
   } else {
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      speed:3000,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      /* pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      }, */
-    });
+    if (sc < 1150) {
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        speed: 3000,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      });
+    } else {
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        speed: 3000,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      });
+    }
   }
 }
 
